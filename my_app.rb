7b3eb81f778ba051c8ede061d44756b4a141c415
Ken Crocken <kcrocken@gmail.com>
2018-05-29 23:27:04 -0400

    adds rack config to heroku deployment

--- my_app.rb
+++ my_app.rb
@@ -14,7 +14,7 @@ configure do
 end
 
 before do
-  if request.body.size > 0
+  if request.body.read(1)
     request.body.rewind
     @values = JSON.parse request.body.read
   end
@@ -24,16 +24,11 @@ before do
 end
 
 def send_email(params, ipaddress)
-  puts ENV['SENDGRID_API_KEY']
-  puts params['name']
-  puts params['email']
-  puts params['message']
-  puts ipaddress
+  puts params
   message = params['message']
-  puts message
   from = Email.new(email: 'no-reply@kencrocken.github.io')
   to = Email.new(email: 'kcrocken@gmail.com')
-  subject = 'Sending with SendGrid is Fun'
+  subject = "[kencrocken.github.io] New Message from #{params['name']} - #{params['email']}"
   content = Content.new(type: 'text/plain', value: message)
   mail = Mail.new(from, subject, to, content)
 
@@ -48,7 +43,7 @@ get '/' do
   { message: 'success', ok: true }.to_json
 end
 post '/' do
-  unless !@values.empty?
+  if @values.empty?
     status 500
     response = { message: 'values are empty', ok: false }
     return response.to_json
@@ -58,18 +53,17 @@ post '/' do
     send_email(@values, @env['REMOTE_ADDR'])
     @sent = true
     return @values.to_json
-  rescue Exception => e
+  rescue StandardError => e
     puts e
     status 500
-    @failure = {ok: false, message: 'Ooops, it looks like something went wrong while attempting to send your email. Mind trying again now or later? :)'}
+    @failure = { message: 'Ooops, it looks like something went wrong while attempting to send your email. Mind trying again now or later? :)', ok: false }
     return @failure.to_json
   end
-
 end
 
 options '*' do
   response.headers["Allow"] = "GET, POST, OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Headers"] = "Content-Type"
   response.headers["Access-Control-Allow-Origin"] = "*"
   200
 end
