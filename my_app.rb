b9933a571a1aa64b1ea7a6bb4b16df012fd50a00
Ken Crocken <kcrocken@gmail.com>
2018-06-03 17:29:03 -0400

    remove all origin header from options, change domain to kencrocken

--- my_app.rb
+++ my_app.rb
@@ -20,13 +20,12 @@ before do
   end
   content_type :json
   headers 'Access-Control-Allow-Origin' => 'https://peaceful-easley-3144c1.netlify.com'
-          # 'Access-Control-Allow-Methods' => ['POST']
 end
 
 def send_email(params, ipaddress)
   puts params
   message = params['message']
-  from = Email.new(email: 'no-reply@kencrocken.github.io')
+  from = Email.new(email: 'no-reply@kencrocken.herokuapp.com')
   to = Email.new(email: 'kcrocken@gmail.com')
   subject = "[kencrocken.github.io] New Message from #{params['name']} - #{params['email']}"
   content = Content.new(type: 'text/plain', value: message)
@@ -63,6 +62,5 @@ end
 options '*' do
   response.headers["Allow"] = "GET, POST, OPTIONS"
   response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-  # response.headers["Access-Control-Allow-Origin"] = "*"
   200
 end
