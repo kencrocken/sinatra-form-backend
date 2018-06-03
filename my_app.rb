f33b0229d7d4671c6e9e143489f9e97f9312c49b
Ken Crocken <kcrocken@gmail.com>
2018-06-03 01:52:32 -0400

    adds CORS back, refactors post success message

--- my_app.rb
+++ my_app.rb
@@ -63,6 +63,6 @@ end
 options '*' do
   response.headers["Allow"] = "GET, POST, OPTIONS"
   response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-  response.headers["Access-Control-Allow-Origin"] = "https://blooming-plateau-13052.herokuapp.com/"
+  response.headers["Access-Control-Allow-Origin"] = "https://peaceful-easley-3144c1.netlify.com/"
   200
 end
